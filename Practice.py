95ade1d6c002c468805fd2580e0e9ba700c57881
Morning-KiNYa <morning.master765@gmail.com>
2024-04-26 19:56:28 +0900

    微修正

--- Practice.py
+++ Practice.py
@@ -8,10 +8,11 @@ wikipedia.set_lang("ja") # 日本語版Wikipediaをセット
 correct=0 # 正解数
 r='' # 問題文
 root=tk.Tk()
+root.title("WikiQuiz")
 root.geometry('400x600')
 index = 1 # 進行状況
 
-answers = [ # 回答欄
+answers = [ # 回答一覧
     '東京駅','神田駅 (東京都)','秋葉原駅','御徒町駅','上野駅','鶯谷駅',
     '日暮里駅','西日暮里駅','田端駅','駒込駅','巣鴨駅','大塚駅 (東京都)',
     '池袋駅','目白駅','高田馬場駅','新大久保駅','新宿駅','代々木駅','原宿駅',
@@ -19,19 +20,22 @@ answers = [ # 回答欄
     '田町駅','浜松町駅','新橋駅','有楽町駅',
 ]
 
+c_num = 0 # 出題カテゴリ
 num = 0 # 問題番号
-s = '' # Wiki本文
-btn=None
-s_btn=None
-combo=None
-start_time=''
-l_result=None
-re_btn=None
+s = '' # 取得したWiki本文
+btn=None # 解答ボタン
+s_btn=None # スタートボタン
+combo=None # 回答選択欄
+start_time='' # タイマー用
+l_result=None # メッセージ文
+re_btn=None # もう一度遊ぶボタン
 
+# 正誤判定を表示
 def clear_result():
     global l_result
     l_result['text']=''
 
+# Wiki本文から問題文を生成し表示
 def creat_que():
     global num,r,s,answers,combo,start_time,l_result
     que.delete(0.,tk.END)
@@ -44,12 +48,13 @@ def creat_que():
     ans_word = str(answers[num])
     if ans_word.endswith(' (東京都)'):
         ans_word.replace(' (東京都)','')
-    r = r.replace(ans_word,'☆☆') # 本文中に正答が入っていれば消去
-    r = r.replace(ans_word[:-1],'☆☆') 
+    r = r.replace(ans_word,'☆☆') # 本文中に解答が入っていれば消去
+    r = r.replace(ans_word[:-1],'☆☆') # 本文中に解答-1文字が入っていれば消去(〇〇駅や〇〇県を消す用)
     que.insert(0.,r)   
 
+# 「答える」ボタンの挙動
 def btn_click():
-    global correct,num,r,answers,l_result,btn,re_btn
+    global correct,num,r,answers,l_result,btn,re_btn,index
     user_ans=combo.get()
     if user_ans==answers[num]:
         correct+=1
@@ -59,8 +64,7 @@ def btn_click():
     else:
         msg='不正解'
         root.after(1500,clear_result)
-    if correct==5:
-        index=2
+    if correct==1:
         clear_time = int(time.time() - start_time)
         clear_minute = clear_time // 60
         clear_second = (clear_time % 3600 % 60)
@@ -69,22 +73,26 @@ def btn_click():
         que.insert(1.0,f'お疲れさまでした! \n今回のタイムは{clear_minute}分{clear_second}秒でした')
         btn.destroy()
         combo.destroy()
+        index=2
         re_btn=tk.Button(text='もう一度あそぶ',font=('Arial',14,'bold'),fg='blue',bg='powderblue',command=game_main)
         re_btn.pack()
 
     l_result['text']=msg
-    #root.after(1500,clear_result)
+    root.after(1500,clear_result)
 
+# スタート画面
 def game_main():
     global correct,r,index,answers,num,s_btn,re_btn
     if index==2:
         que.delete(0.,tk.END)
         re_btn.destroy()
+        l_result.destroy()
     index=0
-    que.insert(1.0,"WikiQuizは、Wikpediaの本文だけをみて、ページタイトルを当てるクイズです。\n\n\n表示される文章に、解答と全く同じ文字列が含まれる場合は「☆☆☆」に置き換わりますが、漢字/ひらがな/カタカナ/ローマ字など表記が異なる場合は置き換わりません。\n\nタイトルを特定できる情報を素早く見つけ、回答欄から正しい答えを選択し、5問正解したらクリアです\n\n\n準備ができたら、出題カテゴリを選択し【スタート】ボタンをクリックしてください。")
+    que.insert(1.0,"WikiQuizは、Wikpediaの本文だけをみて、ページタイトルを当てるクイズです。\n\n\n本文中に、解答と全く同じ文字列が含まれる場合は「☆☆☆」に置き換わりますが、漢字/ひらがな/カタカナ/ローマ字など表記が異なる場合や、解答となるページタイトルが「大塚駅 (東京都)」のように重複を避けるための追記がある場合などは置き換わりません。\nただし、ページタイトルの最後につく「駅」「県」などの接尾語は無視されるため、「東京駅」本文内の「東京」というワードは置き換わります。\n\nまた、画像や図は表示されません。\n\nタイトルを特定できる情報を素早く見つけて正しい答えを選択し、5問正解したらクリアです\n\n\n準備ができたら、出題カテゴリを選択し【スタート】ボタンをクリックしてください。")
     s_btn=tk.Button(text='スタート',font=('Arial',14,'bold'),fg='red',bg='mistyrose',command=game_start)
     s_btn.pack()
 
+# クイズ中の画面表示物を生成+タイマースタート
 def game_start():
     global correct,r,index,answers,num,s,s_btn,start_time,combo,l_result,btn
     index = 1
