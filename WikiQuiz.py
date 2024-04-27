81b1069332b35c82f457c5280efc2b6732ee41dd
Morning-KiNYa <morning.master765@gmail.com>
2024-04-27 20:30:16 +0900

    ランキング登録機能実装

---
+++ WikiQuiz.py
@@ -0,0 +1,186 @@
+import wikipedia
+import random
+import tkinter as tk
+from tkinter import ttk,messagebox
+import time
+import rank_dao
+
+wikipedia.set_lang("ja") # 日本語版Wikipediaをセット
+correct=0 # 正解数
+r='' # 問題文
+root=tk.Tk()
+root.title("WikiQuiz")
+root.geometry('400x600')
+index = 1 # 進行状況
+
+answers = [ # 回答一覧
+    ['東京駅','神田駅 (東京都)','秋葉原駅','御徒町駅','上野駅','鶯谷駅',
+    '日暮里駅','西日暮里駅','田端駅','駒込駅','巣鴨駅','大塚駅 (東京都)',
+    '池袋駅','目白駅','高田馬場駅','新大久保駅','新宿駅','代々木駅','原宿駅',
+    '渋谷駅','恵比寿駅','目黒駅','五反田駅','大崎駅','品川駅','高輪ゲートウェイ駅',
+    '田町駅','浜松町駅','新橋駅','有楽町駅',]
+]
+
+cate = [
+    '山手線の駅'
+] # 出題カテゴリ
+cate_num = 0 # 選択されたカテゴリ
+num = 0 # 問題番号
+s = '' # 取得したWiki本文
+btn=None # 解答ボタン
+s_btn=None # スタートボタン
+combo=None # 回答選択欄
+start_time='' # タイマー用
+l_result=None # メッセージ文
+re_btn=None # もう一度遊ぶボタン
+rank_btn=None # Rankingに登録ボタン
+crear_time=0 # クリア時間
+
+# 正誤判定を表示
+def clear_result():
+    global l_result
+    l_result['text']=''
+
+# Wiki本文から問題文を生成し表示
+def creat_que():
+    global num,r,s,answers,combo,start_time,l_result,cate_num
+    que.delete(0.,tk.END)
+    combo.delete(0, last=None)
+    num = random.randint(0,len(answers[cate_num])-1) # ランダムに正答を決定
+    s = wikipedia.page(answers[cate_num][num]).content # Wiki本文を取得
+    target = 'は、' # 概要の冒頭を削除（読み仮名や英名などを削除）
+    idx = s.find(target)
+    r = s[idx+2:]
+    ans_word = str(answers[cate_num][num])
+    if ans_word.endswith(' (東京都)'):
+        ans_word.replace(' (東京都)','')
+    r = r.replace(ans_word,'☆☆') # 本文中に解答が入っていれば消去
+    r = r.replace(ans_word[:-1],'☆☆') # 本文中に解答-1文字が入っていれば消去(〇〇駅や〇〇県を消す用)
+    que.insert(0.,r)   
+
+# 「答える」ボタンの挙動
+def btn_click():
+    global correct,num,r,answers,l_result,btn,re_btn,index,cate_num,rank_btn,clear_time
+    user_ans=combo.get()
+    if user_ans==answers[cate_num][num]:
+        correct+=1
+        msg='正解!'
+        creat_que()
+        root.after(1500,clear_result)
+    else:
+        msg='不正解'
+        root.after(1500,clear_result)
+    if correct==5:
+        clear_time = int(time.time() - start_time)
+        clear_minute = clear_time // 60
+        clear_second = (clear_time % 3600 % 60)
+        #msg+=f'お疲れさまでした! \n今回のタイムは{clear_minute}分{clear_second}秒でした'
+        que.delete(0.,tk.END)
+        que.insert(1.0,f'お疲れさまでした! \n今回のタイムは{clear_minute}分{clear_second}秒でした')
+        btn.destroy()
+        combo.destroy()
+        index=2
+        re_btn=tk.Button(text='もう一度あそぶ',font=('Arial',14,'bold'),fg='blue',bg='powderblue',command=game_main)
+        re_btn.pack()
+        rank_btn=tk.Button(text='ランキングに登録',font=('Arial',14,'bold'),fg='gold',bg='lemonchiffon',command=rank_entry)
+        rank_btn.pack()
+
+    l_result['text']=msg
+    root.after(1500,clear_result)
+
+# 「ランキングに登録」ボタン
+def rank_entry():
+    global cate_num,cate,clear_time,rank_window,clear_time_str,entry_name
+    messagebox.showinfo('お詫び','この機能はまだ実装されていません')
+    # 登録画面表示
+    rank_window = tk.Toplevel(root)
+    # 親ウィンドウを非アクティブに
+    rank_window.grab_set()
+    # 登録画面をアクティブに
+    rank_window.focus_set()
+    label1=tk.Label(rank_window,text='ランキングに登録',font=('Arial',12),anchor=tk.CENTER)
+    label1.grid(row=0,column=0,columnspan=2)
+    label_name=tk.Label(rank_window,text='Player Name',font=('Arial',12,'bold'),anchor=tk.CENTER)
+    label_name.grid(row=1,column=0)
+    entry_name=tk.Entry(rank_window,font=('Arial',12),justify="center",width=32)
+    entry_name.insert(tk.END,'16文字以内')
+    entry_name.grid(row=1,column=1)
+    entry_name.focus_set()
+    label_category=tk.Label(rank_window,text='Category',font=('Arial',12,'bold'),anchor=tk.CENTER)
+    label_category.grid(row=2,column=0)
+    entry_category=tk.Label(rank_window,text=f'{cate[cate_num]}',font=('Arial',12),justify="center")
+    entry_category.grid(row=2,column=1)
+    label_time=tk.Label(rank_window,text='Time',font=('Arial',12,'bold'),anchor=tk.CENTER)
+    label_time.grid(row=3,column=0)
+    clear_time_str=f'{clear_time // 60}分{(clear_time % 3600 % 60)}秒'
+    entry_time=tk.Label(rank_window,text=clear_time_str,font=('Arial',12),justify="center")
+    entry_time.grid(row=3,column=1)
+    entry_btn=tk.Button(rank_window,text='登録',font=('Arial',14,'bold'),fg='gold',bg='lemonchiffon',command=rank_entry_btn)
+    entry_btn.grid(row=4,column=0,columnspan=2)
+
+def rank_entry_btn():
+    global rank_window,entry_name,cate_num,clear_time_str
+    rank_dao.insert_one(rank_dao.Ranking(entry_name.get(),cate_num,clear_time_str))
+    ok=messagebox.showinfo(title="ランキング登録", message="登録が完了しました")
+    if ok=='ok':
+        rank_window.destroy()
+        game_main()
+
+
+# スタート画面
+def game_main():
+    global correct,r,index,answers,num,s_btn,re_btn,rank_btn
+    if index==2:
+        que.delete(0.,tk.END)
+        re_btn.destroy()
+        rank_btn.destroy()
+        l_result.destroy()
+    index=0
+    que.insert(1.0,"WikiQuizは、Wikpediaの本文だけをみて、ページタイトルを当てるクイズです。\n\n\n本文中に、解答と全く同じ文字列が含まれる場合は「☆☆☆」に置き換わりますが、漢字/ひらがな/カタカナ/ローマ字など表記が異なる場合や、解答となるページタイトルが「大塚駅 (東京都)」のように重複を避けるための追記がある場合などは置き換わりません。\nただし、ページタイトルの最後につく「駅」「県」などの接尾語は無視されるため、「東京駅」本文内の「東京」というワードは置き換わります。\n\nまた、画像や図は表示されません。\n\nタイトルを特定できる情報を素早く見つけて正しい答えを選択し、5問正解したらクリアです\n\n\n準備ができたら、出題カテゴリを選択し【スタート】ボタンをクリックしてください。")
+    s_btn=tk.Button(text='スタート',font=('Arial',14,'bold'),fg='red',bg='mistyrose',command=game_start)
+    s_btn.pack()
+
+# クイズ中の画面表示物を生成+タイマースタート
+def game_start():
+    global correct,r,index,answers,num,s,s_btn,start_time,combo,l_result,btn,category_combo,cate_num
+    index = 1
+    s_btn.destroy()
+    cate_num=category_combo.current()
+    que.insert(1.0,r)
+    combo=ttk.Combobox(root,values=answers[cate_num],font=('Arial',14),justify="center",state="readonly")
+    combo.set("答えを選択")
+    combo.pack()
+    btn=tk.Button(text='答える',font=('Arial',14,'bold'),fg='red',bg='mistyrose',command=btn_click)
+    btn.pack()
+    l_result=tk.Label(text='',font=('Arial',20))
+    l_result.pack()
+    creat_que()
+    start_time = time.time()
+
+
+fnt=('Arial',30)
+title=tk.Label(text='WikiQuiz',font=fnt)
+title.pack(side=tk.TOP)
+frame1 = tk.Frame(root)
+frame1.pack(anchor=tk.CENTER)
+category=tk.Label(frame1,text='出題カテゴリ：',font=('Arial',12))
+category.pack(side=tk.LEFT)
+category_combo=ttk.Combobox(frame1,values=cate,font=('Arial',14),justify="center",state="readonly")
+category_combo.current(0)
+category_combo.pack(side=tk.LEFT)
+que = tk.Text(background='lightblue')
+que.pack(fill=tk.X)
+"""
+#que.insert(1.0,r)
+que.pack()
+combo=ttk.Combobox(root,values=answers,font=('Arial',14),justify="center",state="readonly")
+combo.pack()
+btn=tk.Button(text='答える',font=('Arial',14,'bold'),fg='red',bg='mistyrose',command=btn_click)
+btn.pack()
+l_result=tk.Label(text='',font=('Arial',20))
+l_result.pack()
+creat_que()
+start_time = time.time()
+"""
+game_main()
+root.mainloop()
---
+++ rank_dao.py
@@ -0,0 +1,39 @@
+import pymysql
+
+class Ranking:
+    def __init__(self,player_name,category,clear_time,updated=None,id=None):
+        self.player_name=player_name
+        self.category=category
+        self.clear_time=clear_time
+        self.updated=updated
+        self.id=id
+
+def connect():
+    connection = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        database='wikiquiz',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return connection
+
+def find_all():
+    player_ranking=[]
+    with connect() as con:
+        with con.cursor() as cursor:
+            sql = 'SELECT * FROM ranking ORDER BY crear_time ASC,date DESC'
+            cursor.execute(sql)
+            rs = cursor.fetchall()
+
+            for r in rs:
+                player_ranking.append(Ranking(r['player_name'],r['category'],r['clear_time'],r['updated'],r['id']))
+
+    return player_ranking
+
+def insert_one(Ranking):
+    with connect() as con:
+        with con.cursor() as cursor:
+            sql='INSERT INTO ranking(player_name,category,clear_time,updated) VALUES(%s,%s,%s,now())'
+            cursor.execute(sql,(Ranking.player_name,Ranking.category,Ranking.clear_time))
+        con.commit()
